025449847b940c7edbf0490f7601558e16d7726b
Renzo Sevilha <sevilharenzo@gmail.com>
2021-02-26 15:52:59 -0300

    Realização do primeiro exercício

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_3./exercises.js
@@ -0,0 +1,15 @@
+// Exercício 1
+
+let valor = Math.random() * 10;
+let linha = '*';
+let coluna;
+
+for (let index = 1; index < valor; index += 1) {
+    linha += '*'
+}
+
+for (let index = 1; index <= 5; index += 1) {
+    console.log(linha)
+}
+
+
