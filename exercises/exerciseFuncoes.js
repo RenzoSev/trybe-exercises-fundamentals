ff48263bc14d208bec686b5f89839cd6a5c3149c
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-01 21:39:41 -0300

    Realização do exercício 4 de funções

--- exercises/exerciseFuncoes.js
+++ exercises/exerciseFuncoes.js
@@ -1,19 +1,19 @@
 // 1) Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
 function checkPalindromes(word) {
-    let string = word.toLowerCase()
-    let stringArray = Array.from(string)
-    let stringReverse = '' // Poderia utilizar string.reverse(), mas tentei priorizar mais do que vimos até agora.
-    let checker = false
-   
-    for (index = stringArray.length - 1; index >= 0; index -= 1){
+    let string = word.toLowerCase();
+    let stringArray = Array.from(string);
+    let stringReverse = ''; // Poderia utilizar string.reverse(), mas tentei priorizar mais do que vimos até agora.
+    let checker = false;
+
+    for (index = stringArray.length - 1; index >= 0; index -= 1) {
         stringReverse += stringArray[index]
     }
-    
+
     if (string === stringReverse) {
         checker = true
     }
-    
+
     console.log(checker)
 }
 
@@ -22,11 +22,11 @@ checkPalindromes('Garrafa')
 
 // 2) Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-let numbers = [2, 3, 4, 12, 8, 5, 10, 16]
+let numbers = [2, 3, 4, 12, 8, 5, 10, 16];
 
-function highestInteger (array) {
-    let higher = array[0]
-    for (index = 0; index < array.length; index += 1){
+function checkHighestInteger(array) {
+    let higher = array[0];
+    for (index = 0; index < array.length; index += 1) {
         if (array[index + 1] > higher) {
             higher = array[index + 1]
         }
@@ -38,11 +38,11 @@ highestInteger(numbers)
 
 // 3) Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
-let numbers = [2, 3, 4, 12, 8, 5, 10, 16, -1]
+let numbers = [2, 3, 4, 12, 8, 5, 10, 16, -1];
 
-function smallestInteger (array) {
-    let smaller = array[0]
-    for (index = 0; index < array.length; index += 1){
+function checkSmallestInteger(array) {
+    let smaller = array[0];
+    for (index = 0; index < array.length; index += 1) {
         if (array[index + 1] < smaller) {
             smaller = array[index + 1]
         }
@@ -51,5 +51,21 @@ function smallestInteger (array) {
 }
 
 smallestInteger(numbers)
+
+// 4) Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Estrogonofopolda', 'Cairo', 'Joana'];
+
+function checkHighestString(array) {
+    let name = '';
+    for (index = 0; index < array.length; index += 1) {
+        if (array[index].length > name.length) {
+            name = array[index]
+        }
+    }
+    console.log(name)
+}
+
+checkHighestString(names)
 
 //
