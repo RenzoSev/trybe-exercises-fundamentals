68ea58eda27f38072d24771d61e57f199619ee28
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-23 18:40:43 -0300

    [EX2][parte2]

--- exercises/script2.js
+++ exercises/script2.js
@@ -1,8 +1,20 @@
+// PARTE 1
 const calcRefactorial = (num) => {
     let refactorial = num;
     for (let index = num - 1; index > 0; index -= 1) refactorial *= index;
     return refactorial;
 }
+
 const getRefactorial = num => num === 0 ? 1 : calcRefactorial(num)
 
 console.log(getRefactorial(0));
+
+// PARTE 2
+const longestWord = (string) => {
+    const array = string.split(' ');
+    let word = array[0];
+    for (let string of array) word = string.length > word.length ? string : word
+    return word
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
