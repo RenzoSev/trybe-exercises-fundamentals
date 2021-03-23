85b531bf8f220f8f2b93980f68a6d7022d5ff394
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-23 18:20:49 -0300

    [EX2][parte1 - bônus]

--- exercises/script2.js
+++ exercises/script2.js
@@ -1,7 +1,8 @@
-const getRefactorial = num => {
+const calcRefactorial = (num) => {
     let refactorial = num;
     for (let index = num - 1; index > 0; index -= 1) refactorial *= index;
     return refactorial;
-};
+}
+const getRefactorial = num => num === 0 ? 1 : calcRefactorial(num)
 
-console.log(getRefactorial(4));
+console.log(getRefactorial(0));
