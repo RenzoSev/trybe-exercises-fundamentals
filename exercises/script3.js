6979eededdb7e40da317f90a4e9f42363b701f9a
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-24 17:41:48 -0300

    Criação do espaço para realizar exercício

--- exercises/script3.js
+++
@@ -1,9 +0,0 @@
-let clickCount = 0;
-const button = document.querySelector('#button-count');
-
-const buttonCount = () => button.addEventListener('click', () => {
-    clickCount += 1;
-    document.querySelector('#result').innerText = clickCount;
-})
-
-buttonCount();
