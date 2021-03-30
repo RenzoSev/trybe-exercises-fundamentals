9a47e416ccbcd75842c6973a14ac59af66d6c0af
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-29 21:44:15 -0300

    [BONUS] [ate o 4]

--- exercises/bonus.js
+++ exercises/bonus.js
@@ -25,7 +25,12 @@ const battleMembers = {
 };
 
 const damage = (max, min) => Math.random() * (max - min) + min;
-const dragonDramage = damage(dragon.strength, 15);
-const warriorDamage = damage(warrior.strength * warrior.weaponDmg, warrior.strength);
+const actualMana = (mana) => mana > 15 ? mana - 15 : 'Mana insuficiente';
 
+const dragonDamage = (action) => action(dragon.strength, 15);
+const warriorDamage = (action) => action(warrior.strength * warrior.weaponDmg, warrior.strength);
 
+const mageDamage = (actionDamage, actionMana) => ({
+    damage: actionDamage(mage.intelligence * 2, mage.intelligence),
+    manaSpend: actionMana(mage.mana),
+});
