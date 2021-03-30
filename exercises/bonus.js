4fd6c77751396935ac24962a47e6c8285c3d35bd
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-29 22:37:56 -0300

    finalizado

--- exercises/bonus.js
+++ exercises/bonus.js
@@ -29,8 +29,26 @@ const actualMana = (mana) => mana > 15 ? mana - 15 : 'Mana insuficiente';
 
 const dragonDamage = (action) => action(dragon.strength, 15);
 const warriorDamage = (action) => action(warrior.strength * warrior.weaponDmg, warrior.strength);
-
 const mageDamage = (actionDamage, actionMana) => ({
     damage: actionDamage(mage.intelligence * 2, mage.intelligence),
     manaSpend: actionMana(mage.mana),
 });
+
+const gameActions = {
+    // Crie as HOFs neste objeto.
+    warriorAttack: () => {
+        dragon.healthPoints -= warriorDamage(damage);
+        warrior.damage = warriorDamage(damage);
+    },
+    mageAttack: () => {
+        const mageDM = mageDamage(damage, actualMana);
+        dragon.healthPoints -= mageDM.damage;
+        mage.mana = mageDM.manaSpend;
+    },
+    dragonAttack: () => {
+        mage.healthPoints -= dragonDamage(damage);
+        warrior.healthPoints -= dragonDamage(damage);
+        dragon.damage = dragonDamage(damage);
+    },
+    result: () => battleMembers
+};
