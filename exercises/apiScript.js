3fe7a40b1dc09b245f5755dd04966966990a9570
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-12 22:41:09 -0300

    bonus feito

--- exercises/apiScript.js
+++ exercises/apiScript.js
@@ -1,17 +1,18 @@
-// apiScript.js     
-const API_URL = 'https://icanhazdadjoke.com/';
+// apiScript.js
+const API_URL = "https://icanhazdadjoke.com/";
 
-const getJoke = (joke) => document.querySelector('#jokeTitle').innerText = joke;
+const getJoke = (joke) => document.querySelector("#jokeTitle").innerText = joke;
 
 const fetchJoke = () => {
   const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
+    method: "GET",
+    headers: { Accept: "application/json" },
   };
 
   fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data => getJoke(data.joke));
+    .then((response) => response.json())
+    .then((data) => getJoke(data.joke))
+    .catch(() => console.log("Deu erro!"));
 };
 
 window.onload = () => fetchJoke();
--- exercises/script.js
+++ exercises/script.js
@@ -2,19 +2,26 @@ const getArr = () => {
   return Array.from({ length: 10 }, () => Math.round(Math.random() * 51) ** 2);
 };
 
-const createPromise = () => {
-  return new Promise((resolve, reject) => {
-    const arr = getArr();
-    const sum = arr.reduce((acc, cur) => acc + cur);
-    sum < 8000 ? resolve(sum) : reject();
-  });
+const createPromise = async () => {
+  const arr = getArr();
+  const sum = arr.reduce((acc, cur) => acc + cur);
+  if (sum < 8000) return sum;
+  else throw new Error();
 };
 
-const getPromise = () => {
-  createPromise()
-    .then((sum) => [2, 3, 5, 10].map((num) => (sum / num).toFixed(2)))
-    .then((result) => console.log(result))
-    .catch(() => console.log("É mais de oito mil!"));
+const getSum = (sum) => {
+  const arr = [2, 3, 5, 10];
+  return arr.map((num) => (sum / num).toFixed(2));
+};
+
+const getPromise = async () => {
+  try {
+    const sum = await createPromise();
+    const result = getSum(sum);
+    console.log(result);
+  } catch (error) {
+    console.log('É mais de oito mil!!');
+  }
 };
 
 getPromise();
