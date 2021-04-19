ea513ee4c24c9c0f69cae318882ea90876156976
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-19 12:26:27 -0300

    EX1 - feito

---
+++ exercises/unitarios/EX1/compareObj/compareObj.test.js
@@ -0,0 +1,13 @@
+const { describe, it, expect } = require('@jest/globals');
+const { obj1, obj2, obj3 } = require('./compareObj');
+console.log(obj1, obj2, obj3)
+
+describe('EXERCÍCIO 1 - PARTE 5', () => {
+    it('Compare dois objetos para verificar se são idênticos', () => {
+        expect(obj1).toEqual(obj2);
+    })
+
+    it('Compare dois objetos para verificar se são diferentes', () => {
+        expect(obj1).not.toEqual(obj3);
+    })
+})
