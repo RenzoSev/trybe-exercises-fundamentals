45060e9389abce7a600aa9766a70bcbaf4fb0d41
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-19 11:43:43 -0300

    [PARTE 2] feito

---
+++ exercises/unitarios/myRemove/myRemove.test.js
@@ -0,0 +1,23 @@
+const { describe, expect, it } = require('@jest/globals');
+const myRemove = require('./myRemove');
+
+describe('EXERCÍCIO 2 => Parte 2', () => {
+    const array = [1, 2, 3, 4];
+    
+    it('Verifique se myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+        expect(myRemove([...array], 3)).toEqual([1, 2, 4]);
+    })
+
+    it('Verifique se myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+        expect(myRemove([...array], 3)).not.toEqual([...array]);
+    })
+
+    it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+        myRemove(array, 3);
+        expect(array).toEqual([1, 2, 3, 4]);
+    })
+
+    it('Verifique se myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+        expect(myRemove(array, 5)).toEqual(array);
+    })
+});
--- exercises/unitarios/sum/sum.test.js
+++ exercises/unitarios/sum/sum.test.js
@@ -3,18 +3,18 @@ const sum = require('./sum');
 
 describe('Exercício 1 => PARTE 1', () => {
     it('Teste se o retorno de sum(4, 5) é 9', () => {
-        expect(9).toBe(sum(4, 5))
-    })
+        expect(9).toBe(sum(4, 5));
+    });
 
     it('Teste se o retorno de sum(0, 0) é 0', () => {
-        expect(0).toBe(sum(0, 0))
-    })
+        expect(0).toBe(sum(0, 0));
+    });
 
     it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"', () => {
-        expect(() => { sum(4, '5')}).toThrow();
-    })
+        expect(() => { sum(4, '5') }).toThrow();
+    });
 
     it('Teste se a mensagem de erro é "parameters must be numbers" em sum(4, "5")', () => {
-        expect(() => { sum(4, '5')}).toThrow(/parameters must be numbers/)
-    })
-})
+        expect(() => { sum(4, '5') }).toThrow(/parameters must be numbers/);
+    });
+});
