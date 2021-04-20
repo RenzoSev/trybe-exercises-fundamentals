cb78050ff61041d931fc259b91f218038c3dc096
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-20 19:05:49 -0300

    [EX03]

--- exercises/ex-2-3/getUserName.test.js
+++ exercises/ex-2-3/getUserName.test.js
@@ -12,7 +12,25 @@ describe('EXERCÍCIO 2 - Test the resolve and reject', () => {
     it('should reject', () => {
         expect.assertions(1);
         return getUserName(6).catch(error =>
-            expect(error).toEqual({ error: 'User with 6 not found.'})
+            expect(error).toEqual({
+                error: 'User with 6 not found.'
+            })
         );
+    });
+});
+
+describe('EXERCÍCIO 3 - Test the async/await', () => {
+    it('should return Mark', async () => {
+        const name = await getUserName(4);
+        expect(name).toBe('Mark');
+        expect(name).not.toBe('Paul');
+    });
+
+    it('should reject with an nonexistent id', async () => {
+        try {
+            await getUserName(6);
+        } catch (error) {
+            expect(error).toEqual({error: 'User with 6 not found.'})
+        };
     });
 });
