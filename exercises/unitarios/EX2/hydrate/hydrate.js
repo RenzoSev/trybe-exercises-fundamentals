41a58bc28a49079415c96a4da8c4d73b948b9595
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-20 15:29:19 -0300

    criação do espaço da área de exercício

--- exercises/unitarios/EX2/hydrate/hydrate.js
+++
@@ -1,9 +0,0 @@
-function hydrate(stringDrinks) {
-  const cups = stringDrinks
-  .split('')
-  .reduce((acc, carac) => !isNaN(carac) ? acc + +carac : acc, 0);
-
-  return cups > 1 ? `${cups} copos de água` : '1 copo de água';
-};
-
-module.exports = hydrate;
--- exercises/unitarios/EX2/techlist/techList.js
+++
@@ -1,7 +0,0 @@
-function techList(technologies, profile) {
-    return !technologies.length ? 'Vazio!' : technologies
-    .sort()
-    .map(tech => ({tech, name: profile}));
-}
-
-module.exports = techList;
