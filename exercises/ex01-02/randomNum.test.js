478c3e389a299729a99d3c660b8ab499e9f855b0
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-21 20:16:17 -0300

    [EX2]

--- exercises/ex01-02/randomNum.test.js
+++ exercises/ex01-02/randomNum.test.js
@@ -1,12 +1,24 @@
 const random = require('./randomNum');
 
 describe('PARTE 1 - Function getRandomNum', () => {
-    it('shoul return 10', () => {
+    it('should return 10', () => {
         random.getRandomNum = jest
         .fn()
         .mockReturnValue(10);
 
         expect(random.getRandomNum()).toBe(10);
         expect(random.getRandomNum).toHaveBeenCalled();
+    })
+})
+
+describe('PARTE 2 - Implementations', () => {
+    it('should return the division of 2 nums', () => {
+        random.getRandomNum = jest
+        .fn()
+        .mockImplementationOnce((a, b) => a / b);
+
+        expect(random.getRandomNum(4, 2)).toBe(2);
+        expect(random.getRandomNum).toHaveBeenCalled();
+        expect(random.getRandomNum).toHaveBeenCalledWith(4, 2);
     })
 })
