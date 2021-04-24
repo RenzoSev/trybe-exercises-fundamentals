8f558b71ab899a8a239b4613cca1bcddc6d8b081
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-24 17:32:07 -0300

    [EX 01 02]

---
+++ exercises/hello-world/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const compromissos = ['Acordar', 'Comer', 'Dormir'];
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <ul>
+      {compromissos
+      .map(compromisso => task(compromisso))}
+    </ul>
+  );
+}
+
+export default App;
